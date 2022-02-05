3e30a4d064fbcb297b0132e7fd264f191a4bd323
min33sky <min33sky@naver.com>
2022-02-05 13:06:42 +0900

    :lipstick: modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,28 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col p-5 space-y-2">
-      <div className="flex flex-col">
-        <input
-          type="text"
-          placeholder="Username"
-          className="p-1 border border-gray-400 rounded-lg outline-none peer valid:bg-green-100 invalid:bg-red-200 invalid:ring-2 ring-red-400"
-          required
-        />
-        <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-          This input is invalid
-        </span>
-        <span className="hidden peer-valid:block peer-valid:text-green-500">Awesome Username</span>
+    <div className="flex flex-col p-5 space-y-2">
+      <details className="select-none open:bg-blue-300 ">
+        <summary className="cursor-pointer">What is my fav. food</summary>
+        <p>김치</p>
+        <p>라면</p>
+      </details>
+
+      <div className="p-5">
+        <ul className="list-decimal select-none marker:text-red-600 marker:bg-slate-500 ">
+          <li>안녕</li>
+          <li>하이</li>
+          <li>곤니찌와</li>
+        </ul>
       </div>
 
-      <div className="flex flex-col">
+      <div>
         <input
-          type="password"
-          placeholder="Password"
-          className="p-1 border border-gray-400 rounded-lg outline-none peer valid:bg-green-100 invalid:bg-red-200 invalid:ring-2 ring-red-400"
-          required
+          type="file"
+          className="file:cursor-pointer file:transition-colors file:bg-indigo-500 file:rounded-lg file:text-white file:p-2 file:hover:bg-purple-500"
         />
-        <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-          This password is invalid
-        </span>
-        <span className="hidden peer-valid:block peer-valid:text-green-500">Awesome Password</span>
       </div>
-
-      <input type="submit" value="Login" className="font-bold" />
-    </form>
+    </div>
   );
 };
 
